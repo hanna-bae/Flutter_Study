c99d30395dda0719e22cd44308db0fcefef6c483
hanna-bae <122340442+hanna-bae@users.noreply.github.com>
2023-02-18 00:51:28 +0900

    feature/form_input

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -16,26 +16,88 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        title: Text('MainPage'),
-        actions: [
-          Icon(Icons.search),
-        ],
-      ),
       body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 0.5,
-            color: Colors.black,
-          ),
+        width: 317,
+        height: 77,
+        padding: const EdgeInsets.only(
+          top: 9,
+          bottom: 10,
         ),
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        width: 300.0,
-        height: 300.0,
-        alignment: Alignment.bottomCenter,
-        child: FlutterLogo(size: 100),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 48,
+              height: 17,
+              padding: const EdgeInsets.only(
+                right: 14,
+                top: 1,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Username",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 1),
+            Container(
+              width: 315,
+              height: 45,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color(0xff4caf50),
+                  width: 1,
+                ),
+                color: Colors.white,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 315,
+                    height: 40,
+                    color: Colors.white,
+                    padding: const EdgeInsets.only(
+                      top: 14,
+                      bottom: 13,
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 315,
+                          height: 13.60,
+                          child: Text(
+                            "Your username",
+                            style: TextStyle(
+                              color: Color(0x33000000),
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
